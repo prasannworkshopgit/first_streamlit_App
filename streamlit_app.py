e2a39621cd0bd0bff3332fe6858930269aa705ab
Prasann Patil <117333276+prasannworkshopgit@users.noreply.github.com>
2022-11-04 13:35:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT "prasannpatil","Amazon Web Services","Canada (Central)"")
+my_cur.execute("SELECT prasannpatil,Amazon Web Services,Canada (Central)")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
